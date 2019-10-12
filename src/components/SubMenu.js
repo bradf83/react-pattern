14b639d88e76a473c97fa30ca1a479c2ecbe8a7f
Brad Fontaine <bradf.83@gmail.com>
2019-10-12 15:53:59 -0600

    Added font awesome and a basic indicator that there is a sub menu and if its open or closed.

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,11 +1,16 @@
 import React, {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 
 const SubMenu = ({menuName, children}) => {
     const [expanded, setExpanded] = useState(false);
 
     return (
         <>
-            <button className="list-group-item list-group-item-action" onClick={() => setExpanded(current => !current)}>{menuName}</button>
+            <button className="list-group-item list-group-item-action" onClick={() => setExpanded(current => !current)}>
+                {menuName}
+                <FontAwesomeIcon icon={faCaretRight} className={`fa-lg fa-pull-right ${expanded && 'fa-rotate-90'}`} />
+            </button>
             <div style={{display: `${expanded ? 'block' : 'none'}`}}>
                 {children}
             </div>
