5ae07f6d27ba801f285a2b26ce47e7a8f9310c5b
Brad Fontaine <bradf.83@gmail.com>
2019-10-20 18:23:44 -0600

    Created a very basic "timeline" component.

---
+++ src/components/HorizontalTimeline.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HorizontalTimeline = ({length = 0}) => {
+    if(length <= 0){
+        return null;
+    }
+
+    const handleClick = (index) => {
+        window.alert(`You clicked on item: ${index + 1}`)
+    };
+
+    //TODO: There has to be a better way to do this....just need a sequence of numbers 1 > length
+    const emptyArray = new Array(length).fill(0);
+    return (
+        <ul className="timeline">
+            {emptyArray.map((item, index) =>
+                <li key={index} className={`${(index === length - 1) && 'last'}`} onClick={() => handleClick(index)}/>
+            )}
+        </ul>
+    )
+};
+
+export default HorizontalTimeline;
--- src/components/PublicWelcome.js
+++ src/components/PublicWelcome.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ContainerContent from "./ContainerContent";
+import HorizontalTimeline from "./HorizontalTimeline";
 
 const PublicWelcome = () => {
     return (
         <ContainerContent>
             <h5>Welcome</h5>
             <p>Another React app!  Time to learn some more stuff.</p>
+
+            <div>
+                <HorizontalTimeline length={15}/>
+            </div>
         </ContainerContent>
     )
 };
