1d02eec2981258f1b585c14538f5f515d553530e
Brad Fontaine <bradf.83@gmail.com>
2019-10-26 11:11:41 -0600

    Added some tooltip examples using react-tooltip.

--- src/components/PublicWelcome.js
+++ src/components/PublicWelcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContainerContent from "./ContainerContent";
 import HorizontalTimeline from "./HorizontalTimeline";
+import ReactTooltip from 'react-tooltip'
 
 const PublicWelcome = () => {
     return (
@@ -8,9 +9,22 @@ const PublicWelcome = () => {
             <h5>Welcome</h5>
             <p>Another React app!  Time to learn some more stuff.</p>
 
+            <h6>Timeline Examples</h6>
+
             <div>
                 <HorizontalTimeline length={15}/>
             </div>
+
+            <h6>Tooltip Examples - with <em>React Tooltip</em></h6>
+
+            <div>
+                <button className="btn btn-primary ml-2" data-place="top" data-tip="Primary Tooltip on Top">Primary</button>
+                <button className="btn btn-secondary ml-2"  data-place="bottom" data-tip="Secondary Tooltip on Bottom">Secondary</button>
+                <button className="btn btn-danger ml-2" data-place="left" data-tip="Danger Tooltip on the Left">Danger</button>
+                <button className="btn btn-info ml-2" data-place="right" data-tip="Info Tooltip on the Right">Info</button>
+            </div>
+            <ReactTooltip />
+
         </ContainerContent>
     )
 };
